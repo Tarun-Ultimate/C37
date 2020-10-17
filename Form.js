3188fc7b9039245932cccf12f6eeb93b9a26facb
Tarun-Ultimate <68709064+Tarun-Ultimate@users.noreply.github.com>
2020-10-16 19:55:11 -0500

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,35 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+  }
+
+  display(){
+    var title = createElement('h2')
+    title.html("Car Racing Game");
+    title.position(720, 0);
+
+    this.input.position(720, 75);
+    this.button.position(850, 110);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(130, 100);
+    });
+
+  }
+}
